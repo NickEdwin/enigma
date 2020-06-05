f0ee2f66189856fdf2cd4f13a50784c8151bf132
Nick Edwin <nickedwin85@gmail.com>
2020-06-05 10:49:06 -0600

    Added attributes to enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,8 +2,19 @@ require_relative 'key'
 require_relative 'offset'
 
 class Enigma
+  attr_reader :message,
+              :key,
+              :date
+              
+  def initialize(message, key, date)
+    @message = message
+    @key = key
+    @date = date
+  end
 
   def alphabet
     ("a".."z").to_a << " "
   end
+
+
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,13 +5,19 @@ require './lib/enigma'
 class EnigmaTest < Minitest::Test
 
   def test_it_exists
-    enigma = Enigma.new
+    enigma = Enigma.new("message", "key", "date")
     assert_instance_of Enigma, enigma
   end
 
-  def test_it_finds_full_alphabet
-    enigma = Enigma.new
-    assert_equal 27, enigma.alphabet.length
+  def test_it_has_attributes
+    enigma = Enigma.new("message", "key", "date")
+    assert_equal "message", enigma.message
+    assert_equal "key", enigma.key
+    assert_equal "date", enigma.date
+  end
 
+  def test_it_finds_full_alphabet
+    enigma = Enigma.new("message", "key", "date")
+    assert_equal 27, enigma.alphabet.length
   end
 end
