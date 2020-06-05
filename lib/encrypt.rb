fe7dbccf281f86c11e55c519d24f70ed55ee2cfe
Nick Edwin <nickedwin85@gmail.com>
2020-06-04 20:51:57 -0600

    Generated 5 digit key

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,4 +1,8 @@
 require 'date'
 
 class Encrypt
+
+  def key
+    '%05d' % rand(0..99999)
+  end
 end
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -8,4 +8,10 @@ class EncryptTest < Minitest::Test
     encrypt = Encrypt.new
     assert_instance_of Encrypt, encrypt
   end
+
+  def test_it_generates_five_digit_key
+    encrypt = Encrypt.new
+    assert_equal 5, encrypt.key.length
+  end
+
 end
