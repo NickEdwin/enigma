7d29f3f9d709024cf39fb377b40fffcbf29f9906
Nick Edwin <nickedwin85@gmail.com>
2020-06-05 10:36:13 -0600

    Fixed final_offset to be single digit

--- lib/offset.rb
+++ lib/offset.rb
@@ -12,10 +12,10 @@ class Offset
   def final_offset
     date = generate_offset
     final_offset = Hash.new(0)
-    final_offset = { A: date[0..1],
-                     B: date[1..2],
-                     C: date[2..3],
-                     D: date[3..4]
+    final_offset = { A: date[0],
+                     B: date[1],
+                     C: date[2],
+                     D: date[3]
                    }
   end
 end
