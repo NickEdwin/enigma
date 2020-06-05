5c5fd8a359354568b35d6f547691f2d3f618b0ff
Nick Edwin <nickedwin85@gmail.com>
2020-06-05 10:31:56 -0600

    Updated offset class to create final_offset hash

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,7 +1,21 @@
-# def todays_date
-#   Time.now.strftime("%d%m%y")
-# end
-#
-# def offset
-#   date = todays_date.to_i ** 2).to_s.slice(-4..-1)
-# end
+require 'date'
+
+class Offset
+  def todays_date
+    Time.now.strftime("%d%m%y")
+  end
+
+  def generate_offset
+    (todays_date.to_i ** 2).to_s.slice(-4..-1)
+  end
+
+  def final_offset
+    date = generate_offset
+    final_offset = Hash.new(0)
+    final_offset = { A: date[0..1],
+                     B: date[1..2],
+                     C: date[2..3],
+                     D: date[3..4]
+                   }
+  end
+end
