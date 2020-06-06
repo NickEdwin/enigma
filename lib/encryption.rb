f87881b1e4870d892cea3a30a5d17c499902ec75
Nick Edwin <nickedwin85@gmail.com>
2020-06-06 13:06:39 -0600

    Added decryption method and test

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -31,6 +31,13 @@ class Encryption
     input.downcase.split(//)
   end
 
+  def  encryption_alphabet_guard
+    new_index = 0
+    if new_index > 27
+      new_index = new_index % 27
+    end
+  end
+
   def encryption(string)
     index_counter = 0
     return_string = ""
@@ -40,9 +47,7 @@ class Encryption
         index_counter += 1
       elsif index_counter == 0 || index_counter % 4 == 0
         new_index = alphabet.index(letter) + shift[:A]
-          if new_index > 27
-            new_index = new_index % 27
-          end
+        #/
         return_string += alphabet[new_index]
         index_counter += 1
       elsif index_counter == 1 || index_counter % 4 == 1
@@ -66,8 +71,48 @@ class Encryption
           end
         return_string += alphabet[new_index]
         index_counter += 1
-    end
+        end
+      end
+    return_string
   end
-  return_string
-end
+
+  def decryption(string)
+    index_counter = 0
+    return_string = ""
+    split_input(string).each do |letter|
+      if !alphabet.include?(letter)
+        return_string += letter
+        index_counter += 1
+      elsif index_counter == 0 || index_counter % 4 == 0
+        new_index = alphabet.index(letter) - shift[:A]
+          if new_index < 27
+            new_index = new_index % 27
+          end
+        return_string += alphabet[new_index]
+        index_counter += 1
+      elsif index_counter == 1 || index_counter % 4 == 1
+        new_index = alphabet.index(letter) - shift[:B]
+          if new_index < 27
+            new_index = new_index % 27
+          end
+        return_string += alphabet[new_index]
+        index_counter += 1
+      elsif index_counter == 2 || index_counter % 4 == 2
+        new_index = alphabet.index(letter) - shift[:C]
+          if new_index < 27
+            new_index = new_index % 27
+          end
+        return_string += alphabet[new_index]
+        index_counter += 1
+      elsif index_counter == 3 || index_counter % 4 == 3
+        new_index = alphabet.index(letter) - shift[:D]
+          if new_index < 27
+            new_index = new_index % 27
+          end
+        return_string += alphabet[new_index]
+        index_counter += 1
+        end
+      end
+    return_string
+  end
 end
--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -44,6 +44,10 @@ class EncryptionTest < Minitest::Test
   def test_it_ecrypts_string
     encryption = Encryption.new("Hello World!", "02715", "040895")
     assert_equal "keder ohulw!", encryption.encryption(encryption.string)
+  end
 
+  def test_it_decrypts_String
+    encryption = Encryption.new("keder ohulw!", "02715", "040895")
+    assert_equal "hello world!", encryption.decryption(encryption.string)
   end
 end
