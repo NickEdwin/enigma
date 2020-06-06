ddaef1b6805e27dd0b7ce833c21e75327463c0b9
Nick Edwin <nickedwin85@gmail.com>
2020-06-06 16:35:57 -0600

    Added decryption tests and method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -9,4 +9,9 @@ class Enigma
     message = Encryption.new(message, key, date)
     message.encryption_result
   end
+
+  def decrypt(message, key = rand(0..99999).to_s.ljust(5, '0'), date = Time.new.strftime("%d%m%y"))
+    message = Encryption.new(message, key, date)
+    message.decryption_result
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -24,4 +24,20 @@ class EnigmaTest < Minitest::Test
     enigma = Enigma.new
     assert_equal 5, enigma.encrypt("Hello World!").values[1].length
   end
+
+  def test_it_encrypts_message_with_known_input
+    enigma = Enigma.new
+    expected = {decryption: "hello world!", key: "02715", date: "040895"}
+    assert_equal expected, enigma.decrypt("keder ohulw!", "02715", "040895")
+  end
+
+  def test_it_decrypts_message_with_todays_date
+    enigma = Enigma.new
+    assert_equal Time.new.strftime("%d%m%y"), enigma.decrypt("Hello World!", "02715").values[2]
+  end
+
+  def test_it_decrypts_message_with_random_key_and_date
+    enigma = Enigma.new
+    assert_equal 5, enigma.decrypt("Hello World!").values[1].length
+  end
 end
